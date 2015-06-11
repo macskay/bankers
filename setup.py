fe9c91b8b9e8f2346bf881396038bd29bb9521fd
Max <M.Klingmann@stud.uni-heidelberg.de>
2015-06-11 12:45:53 +0200

    Pushed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name="bankers",
-      version="0.01",
+      version="0.0.1",
       description="Bankers - This is an implementation of the banker's deadlock algorithm",
       author="Max Klingmann <KlingmannM@gmail.com>",
       author_email="KlingmannM@gmail.com",
